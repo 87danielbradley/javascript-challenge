67395731e4440e4c3ef5a5b9a673e28703896cc2
Daniel Bradley <87danielbradley@gmail.com>
2020-05-17 22:29:27 -0400

    level 2 completed

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,56 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(ufoReport => {
+	var row = tbody.append("tr");
+	row.append("td").text(ufoReport.datetime);
+	row.append("td").text(ufoReport.city);
+	row.append("td").text(ufoReport.state);
+	row.append("td").text(ufoReport.country);
+	row.append("td").text(ufoReport.shape);
+	row.append("td").text(ufoReport.durationMinutes);
+	row.append("td").text(ufoReport.comments);
+});
+
+var button = d3.select("#filter-btn");
+
+
+
+
+
+button.on("click", runEnter);
+
+
+function runEnter() {
+	
+	
+	d3.event.preventDefault();
+	tbody.html("");
+	var inputElement = d3.select("#datetime");
+	
+	var inputValue = inputElement.property("value");
+	
+	console.log(inputValue);
+	console.log(tableData);
+	var filteredData = tableData.filter( ufo => ufo.datetime === inputValue);
+	
+	console.log(filteredData);
+	
+	filteredData.forEach(ufoReport => {
+		var filteredRow = tbody.append("tr");
+		filteredRow.append("td").text(ufoReport.datetime);
+		filteredRow.append("td").text(ufoReport.city);
+		filteredRow.append("td").text(ufoReport.state);
+		filteredRow.append("td").text(ufoReport.country);
+		filteredRow.append("td").text(ufoReport.shape);
+		filteredRow.append("td").text(ufoReport.durationMinutes);
+		filteredRow.append("td").text(ufoReport.comments);
+	});
+	
+	
+};
+
+
+
